ab73f4c7c23115047fd12aab20ca243683abbffc
Marreco-max <vibarkov5@gmail.com>
2025-06-03 10:10:49 -0400

    Update 2121.js
    up

--- 2121.js
+++ 2121.js
@@ -1,5 +1,16 @@
-// scripts.js
-
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Página carregada com sucesso!');
+
+    const openBtn = document.getElementById('openSidebar');
+    const closeBtn = document.getElementById('closeSidebar');
+    const sidebar = document.getElementById('sidebar');
+
+    openBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        sidebar.classList.add('open');
+    });
+
+    closeBtn.addEventListener('click', () => {
+        sidebar.classList.remove('open');
+    });
 });
